a89d0fe6ec2da45c04c46935b57cb819749a9a9a
chrissschin <solischris92@gmail.com>
2015-09-26 03:12:49 -0700

    added jquery events

--- js/app.js
+++ js/app.js
@@ -1,4 +1,14 @@
 $(document).ready(function(){
-  // $(".box2").append("sup");
-
-})
+  //when button is clicked
+  $('.addbutton').click(function(){
+    //store item in var
+    var listitem = $("#item");
+    //append item to box2 as list item
+    $(".box2 ul").append("<li>" + listitem.val() + "</li>");
+    listitem.val('').focus();
+  });
+  //resets list
+  $('.resetbutton').click(function(){
+    $('.box2 ul li').remove();
+  });
+});
